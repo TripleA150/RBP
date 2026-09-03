ec73877719023ddbceb2a1dc259ba9d27cd7c499
CluVPN <46062533+TripleA150@users.noreply.github.com>
2026-09-03 09:30:20 +0700

    add missing files

--- setup/root/antizapret/parse.sh
+++ setup/root/antizapret/parse.sh
@@ -23,6 +23,25 @@ rm -rf temp result
 mkdir -p temp result
 source setup
 
+###
+mv -f config/rpz.txt config/deny.txt 2>/dev/null || true
+mv -f config/rpz2.txt config/deny2.txt 2>/dev/null || true
+mv -f config/deny.txt config/deny-rpz.txt 2>/dev/null || true
+mv -f config/deny2.txt config/deny2-rpz.txt 2>/dev/null || true
+mv -f config/proxy.txt config/proxy-rpz.txt 2>/dev/null || true
+
+if [[ -f "config/include-hosts.txt" ]] && ! grep -qF 'добавление всех доменов' config/include-hosts.txt; then
+	sed -i '/^#   xn--80aswg\.xn--p1ai/a\#   .                      - добавление всех доменов (исключения задаются в config/exclude-hosts.txt)' config/include-hosts.txt
+fi
+
+if [[ ! -f "config/proxy-rpz.txt" ]]; then
+	echo '; Настройка RPZ для маршрутизации через AntiZapret VPN
+; https://www.knot-resolver.cz/documentation/latest5/modules-policy.html#response-policy-zones
+; CNAME . добавляет домен для маршрутизации через AntiZapret VPN
+;' > config/proxy-rpz.txt
+fi
+###
+
 for file in config/*.txt; do
 	sed -i -e '$a\' "$file"
 done
